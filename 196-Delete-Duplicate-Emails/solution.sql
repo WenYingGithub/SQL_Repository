2098a0311f355533238ff30a0f8e65c9fa60e7b8
YingWen2016 <ywen03@syr.edu>
2016-07-15 12:48:23 -0400

    [196. Delete Duplicate Emails][Accepted]committed by LeetCode Extension

---
+++ 196-Delete-Duplicate-Emails/solution.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+DELETE FROM Person
+    WHERE Id NOT IN (SELECT Id 
+                            FROM( 
+                                  SELECT MIN(Id) AS Id 
+                                        FROM Person 
+                                        GROUP BY Email
+                                 )tem
+                     );
